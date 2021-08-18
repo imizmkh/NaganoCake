8a8ffc0c63d021430db433e2416cd29e28f3e4c3
EC2 Default User <ec2-user@ip-172-31-5-227.ec2.internal>
2021-08-18 13:37:33 +0000

    [add]products

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,12 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :products
+
+  end
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
