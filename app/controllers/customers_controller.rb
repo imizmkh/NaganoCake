1d30620a5c41c4bf891aa19ae26510ce09a4a33b
EC2 Default User <ec2-user@ip-172-31-5-227.ec2.internal>
2021-08-30 13:02:02 +0000

    [add]nagano_cake end exit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,26 @@
 class CustomersController < ApplicationController
+  before_action :set_cart_item, only: [:add_item, :destroy]
+  before_action :set_user
+
 
   def show
     @customer = current_customer
+    @cart_items = @customer.cart_items
+  end
+
+  def add_item
+    @cart_item = @customer.cart_items.build(product_id: params[:product_id]) if @cart_item.blank?
+    @cart_item.amount += params[:amount].to_i
+    if @cart_item.save
+      redirect_to current_customer
+    else
+      redirect_to controller: "products", action: "show"
+    end
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to curent_customer
   end
 
   def edit
@@ -29,6 +48,14 @@ class CustomersController < ApplicationController
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
+  def set_customer
+    @customer =current_customer
+  end
+  
+  def set_cart_item
+    @cart_item = current_customer.cart_items.find_by(product_id: params[:product_id])
   end
 
 
